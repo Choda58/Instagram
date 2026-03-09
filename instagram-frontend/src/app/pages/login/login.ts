07087538848c5d4d507f789a52be68bf05762f04
Sanja <sanjaprvlovic11@gmail.com>
2026-03-09 16:26:27 +0100

    Implement login and register pages with styling and routing

--- instagram-frontend/src/app/pages/login/login.ts
+++ instagram-frontend/src/app/pages/login/login.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  username: string = '';
+  password: string = '';
+
+  constructor(private userService: UserService) {}
+
+  login(): void {
+
+    const data = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.userService.login(data).subscribe(response => {
+      console.log('Login response:', response);
+    });
+
+  }
+
+}
--- instagram-frontend/src/app/pages/register/register.ts
+++ instagram-frontend/src/app/pages/register/register.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterModule],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrls: ['./register.css'],
 })
-export class Register {}
+export class Register {
+
+  username: string = '';
+  email: string = '';
+  password: string = '';
+
+  constructor(private userService: UserService) {}
+
+  register(): void {
+
+    const user = {
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    this.userService.register(user).subscribe(response => {
+      console.log('Register response:', response);
+    });
+
+  }
+
+}
