d53d865047d5adf2399647735d58a47bedf1f41d
Sanja <sanjaprvlovic11@gmail.com>
2026-03-09 11:32:15 +0100

    Initialize Angular frontend with pages, services and models

---
+++ instagram-frontend/src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { Login } from '../models/login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = 'http://localhost:8081/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  register(user: User): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/register`, user);
+  }
+
+  login(data: Login): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, data);
+  }
+
+}
