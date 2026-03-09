94256328380588956e0b9953d4ec2e4501a777d6
Sanja <sanjaprvlovic11@gmail.com>
2026-03-09 18:51:31 +0100

    Implement navigation bar component with styling

--- instagram-frontend/src/app/app.ts
+++ instagram-frontend/src/app/app.ts
@@ -1,12 +1,37 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('instagram-frontend');
+
+  showNavbar = true;
+
+  constructor(private router: Router) {
+
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationEnd) {
+
+        const url = event.url;
+
+        if (url === '/login' || url === '/register') {
+          this.showNavbar = false;
+        } else {
+          this.showNavbar = true;
+        }
+
+      }
+
+    });
+
+  }
+
 }
